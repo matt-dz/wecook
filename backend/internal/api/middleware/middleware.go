1bb665cd06a3c1a68cd6782348a41e07eeeaea00
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2025-11-30 20:49:43 -0500

    fix: add credentials header for cors policy

--- backend/internal/api/middleware/middleware.go
+++ backend/internal/api/middleware/middleware.go
@@ -72,6 +72,7 @@ func AddCors(next http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Add("Access-Control-Max-Age", "86400")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusNoContent)
