47be94d06bd57bc2536c2d9439bcfadd2c851597
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2025-12-27 15:30:27 -0500

    feat: implement settings scaffold page

--- frontend/src/routes/(admin)/admin/settings/+page.svelte
+++ frontend/src/routes/(admin)/admin/settings/+page.svelte
@@ -1,1 +1,18 @@
-<h1>settings</h1>
+<script lang="ts">
+	import { Switch } from '$lib/components/ui/switch/index.js';
+	import { Label } from '$lib/components/ui/label/index.js';
+</script>
+
+<div class="mt-12 flex justify-center">
+	<div class="flex w-full max-w-3xl justify-center">
+		<div class="flex items-start gap-2">
+			<Switch id="public-signup" disabled />
+			<div class="space-y-1">
+				<Label for="public-signup" class="font-inter">Public Signup</Label>
+				<p class="font-inter text-sm font-light text-gray-400">
+					If disabled (default), users must receive an invitation to signup.
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
